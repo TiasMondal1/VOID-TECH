997587c2a4a3a36e92e0e973845891377b668d3c
ronie-18 <rounakjana74@gmail.com>
2023-09-19 21:20:56 +0530

    Fake

--- not actual/script.js
+++ not actual/script.js
@@ -12,3 +12,12 @@ navLinks.forEach((navLinks) => {
         hamMenuIcon.classList.toggle("fa-times");
     });
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
